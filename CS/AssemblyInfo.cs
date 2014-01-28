3292f5a695c1a836204a358df1446887851c1c53
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2014-01-28 11:33:12 +0100

    double checked that this version corresponds with florian's

--- CS/AssemblyInfo.cs
+++ CS/AssemblyInfo.cs
@@ -1,21 +1,21 @@
 #region Header
 //
-// Copyright 2003-2014 by Autodesk, Inc. 
+// Copyright 2003-2014 by Autodesk, Inc.
 //
 // Permission to use, copy, modify, and distribute this software in
-// object code form for any purpose and without fee is hereby granted, 
-// provided that the above copyright notice appears in all copies and 
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
 // that both that copyright notice and the limited warranty and
-// restricted rights notice below appear in all supporting 
+// restricted rights notice below appear in all supporting
 // documentation.
 //
-// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
 // AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
-// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC. 
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
 // DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
 // UNINTERRUPTED OR ERROR FREE.
 //
-// Use, duplication, or disclosure by the U.S. Government is subject to 
+// Use, duplication, or disclosure by the U.S. Government is subject to
 // restrictions set forth in FAR 52.227-19 (Commercial Computer
 // Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
 // (Rights in Technical Data and Computer Software), as applicable.
@@ -38,26 +38,26 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture( "" )]
 
 //
-// In order to sign your assembly you must specify a key to use. Refer to the 
+// In order to sign your assembly you must specify a key to use. Refer to the
 // Microsoft .NET Framework documentation for more information on assembly signing.
 //
-// Use the attributes below to control which key is used for signing. 
+// Use the attributes below to control which key is used for signing.
 //
-// Notes: 
+// Notes:
 //   (*) If no key is specified, the assembly is not signed.
 //   (*) KeyName refers to a key that has been installed in the Crypto Service
 //       Provider (CSP) on your machine. KeyFile refers to a file which contains
 //       a key.
-//   (*) If the KeyFile and the KeyName values are both specified, the 
+//   (*) If the KeyFile and the KeyName values are both specified, the
 //       following processing occurs:
 //       (1) If the KeyName can be found in the CSP, that key is used.
-//       (2) If the KeyName does not exist and the KeyFile does exist, the key 
+//       (2) If the KeyName does not exist and the KeyFile does exist, the key
 //           in the KeyFile is installed into the CSP and used.
 //   (*) In order to create a KeyFile, you can use the sn.exe (Strong Name) utility.
 //       When specifying the KeyFile, the location of the KeyFile should be
 //       relative to the project output directory which is
 //       %Project Directory%\obj\<configuration>. For example, if your KeyFile is
-//       located in the project directory, you would specify the AssemblyKeyFile 
+//       located in the project directory, you would specify the AssemblyKeyFile
 //       attribute as [assembly: AssemblyKeyFile("..\\..\\mykey.snk")]
 //   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
 //       documentation for more information on this.
@@ -79,5 +79,6 @@ using System.Runtime.CompilerServices;
 // 2013-10-24 - 2014.0.0.5 - merged pull request from Prasadgalle
 // 2014-01-11 - 2014.0.0.6 - joespiff adjusted the Anchor property of the "Built-in Enums Map..." button on the Parameters form so that it behaves well when stretching the form
 // 2014-01-27 - 2014.0.0.7 - merged pull request from FlorianSchmid of SOFiSTiK: extended (added) snooping of geometry, FormatOptions and RevitLinkInstances plus some fixes of compiler errors/warnings; bumped copyright year from 2013 to 2014
-[assembly: AssemblyVersion( "2014.0.0.7" )]
-[assembly: AssemblyFileVersion( "2014.0.0.7" )]
+// 2014-01-28 - 2014.0.1.0 - double checked that this version corresponds with florian's
+[assembly: AssemblyVersion( "2014.0.1.0" )]
+[assembly: AssemblyFileVersion( "2014.0.1.0" )]
