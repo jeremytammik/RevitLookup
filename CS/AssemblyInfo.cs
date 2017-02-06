81d64da94a61a78eedf5cb79d533528fd62f5ec2
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2017-02-06 14:56:11 +0100

    merged pull request #23 from awmcc90 to catch specific reflection invocation exceptions, not all

--- CS/AssemblyInfo.cs
+++ CS/AssemblyInfo.cs
@@ -115,6 +115,7 @@ using System.Runtime.CompilerServices;
 // 2017-02-02 - 2017.0.0.12 - merged pull #21 from @eibre adding UnitType property on the parameter Definition class
 // 2017-02-02 - 2017.0.0.13 - whitespace
 // 2017-02-06 - 2017.0.0.14 - merged pull request #22 from awmcc90 drastic changes implementing object inspection via reflection and cross-version compatibility
+// 2017-02-06 - 2017.0.0.15 - merged pull request #23 from awmcc90 to catch specific reflection invocation exceptions, not all
 //
-[assembly: AssemblyVersion( "2017.0.0.14" )]
-[assembly: AssemblyFileVersion( "2017.0.0.14" )]
+[assembly: AssemblyVersion( "2017.0.0.15" )]
+[assembly: AssemblyFileVersion( "2017.0.0.15" )]
