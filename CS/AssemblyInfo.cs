42397500a0d3c394af2fe617b4ae16e43275afa0
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2017-02-17 10:58:57 +0100

    merged pull request #25 from chekalin-v: fix old bugs, significant improvements to the new reflection approach

--- CS/AssemblyInfo.cs
+++ CS/AssemblyInfo.cs
@@ -116,6 +116,7 @@ using System.Runtime.CompilerServices;
 // 2017-02-02 - 2017.0.0.13 - whitespace
 // 2017-02-06 - 2017.0.0.14 - merged pull request #22 from awmcc90 drastic changes implementing object inspection via reflection and cross-version compatibility
 // 2017-02-06 - 2017.0.0.15 - merged pull request #23 from awmcc90 to catch specific reflection invocation exceptions, not all
+// 2017-02-17 - 2017.0.0.16 - merged pull request #25 from chekalin-v: fix old bugs, significant improvements to the new reflection approach
 //
-[assembly: AssemblyVersion( "2017.0.0.15" )]
-[assembly: AssemblyFileVersion( "2017.0.0.15" )]
+[assembly: AssemblyVersion( "2017.0.0.16" )]
+[assembly: AssemblyFileVersion( "2017.0.0.16" )]
