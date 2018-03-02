c0566f907095f70930a7a855684c2aaff5d22399
Jeremy Tammik <jeremy.tammik@autodesk.com>
2018-03-02 11:19:07 +0100

    removed unused namespaces

--- CS/Snoop/CollectorExts/ElementMethodsStream.cs
+++ CS/Snoop/CollectorExts/ElementMethodsStream.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Windows.Forms;
-using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
 namespace RevitLookup.Snoop.CollectorExts
