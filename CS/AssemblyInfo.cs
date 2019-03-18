cb1ee6c390fdb46f25e5a30f5488618b760f1f4f
Jeremy Tammik <jeremy.tammik@autodesk.com>
2019-03-18 23:34:30 +0100

    unused namespace

--- CS/AssemblyInfo.cs
+++ CS/AssemblyInfo.cs
@@ -23,7 +23,6 @@
 #endregion // Header
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
