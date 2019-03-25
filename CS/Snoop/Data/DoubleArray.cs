b915196e0981e5802b2bf81450ee562077e6158e
Victor Chekalin <chekalin-v@yandex.ru>
2019-03-25 18:35:30 +0500

    Pushed the missed files (#49)
    * Snoop rendering AssetProperty
    
    Material->AppearanceAssetId->GetRenderingAssset
    
    * Missed files

---
+++ CS/Snoop/Data/DoubleArray.cs
@@ -0,0 +1,25 @@
+﻿namespace RevitLookup.Snoop.Data
+{
+    public class DoubleArray : Data
+    {
+        protected Autodesk.Revit.DB.DoubleArray m_val;
+
+        public DoubleArray(string label, Autodesk.Revit.DB.DoubleArray val) : 
+            base(label)
+        {
+            m_val = val;
+        }
+
+        public override string StrValue()
+        {
+            string[] array = new string[m_val.Size];
+
+            for (int i = 0; i < m_val.Size; i++)
+            {
+                array[i] = m_val.get_Item(i).ToString();
+            }
+
+            return string.Join("; ", array);
+        }
+    }
+}
