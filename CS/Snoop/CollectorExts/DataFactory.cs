835a57faa09f5c4994dc257533292d4810c43684
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2019-04-24 00:03:55 +0300

    extract data factory and simplify the code of ElementMethodsStream class

---
+++ CS/Snoop/CollectorExts/DataFactory.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Reflection;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+namespace RevitLookup.Snoop.CollectorExts
+{
+    public class DataFactory
+    {
+        private readonly UIApplication application;
+        private readonly object elem;
+
+        public DataFactory(UIApplication application, object elem)
+        {
+            this.application = application;
+            this.elem = elem;
+        }
+
+        public Data.Data Create(MethodInfo methodInfo)
+        {
+            var declaringType = methodInfo.DeclaringType;
+
+            if (methodInfo.IsSpecialName || declaringType == null)
+                return null;
+
+            if (methodInfo.Name == nameof(Element.GetDependentElements))
+            {
+                var element = (Element) elem;
+
+                return DataTypeInfoHelper.CreateFrom(application, methodInfo, element.GetDependentElements(null), element);
+            }
+
+            if (methodInfo.Name == nameof(Reference.ConvertToStableRepresentation))
+            {
+                var reference = (Reference)elem;
+
+                return DataTypeInfoHelper.CreateFrom(application, methodInfo, reference.ConvertToStableRepresentation(application.ActiveUIDocument.Document), reference);
+            }
+
+            if (declaringType == typeof (Document) && methodInfo.Name == nameof(Document.Close))
+                return null;
+
+            if (methodInfo.GetParameters().Any() || methodInfo.ReturnType == typeof (void))
+                return null;
+
+            var returnValue = methodInfo.Invoke(elem, new object[0]);
+
+            return DataTypeInfoHelper.CreateFrom(application, methodInfo, returnValue, elem);
+        }
+    }
+}
--- CS/Snoop/CollectorExts/ElementMethodsStream.cs
+++ CS/Snoop/CollectorExts/ElementMethodsStream.cs
@@ -3,110 +3,67 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
 namespace RevitLookup.Snoop.CollectorExts
 {
     public class ElementMethodsStream : IElementStream
     {
-        private readonly UIApplication application;
         private readonly ArrayList data;
-        private readonly object elem;
         private readonly List<string> seenMethods = new List<string>();
-        private readonly IEnumerable<string> specialMethods;
-        private readonly IEnumerable<string> forbiddenMethods;
+        private readonly DataFactory methodDataFactory;
 
         public ElementMethodsStream(UIApplication application, ArrayList data, object elem)
         {
-            this.application = application;
             this.data = data;
-            this.elem = elem;
 
-            specialMethods = new[]
-                {
-                    $"{typeof(Reference).FullName}.{nameof(Reference.ConvertToStableRepresentation)}",
-                    $"{typeof(Element).FullName}.{nameof(Element.GetDependentElements)}"
-                };
-
-            forbiddenMethods = new[]
-                {
-                    $"{typeof(Document).FullName}.{nameof(Document.Close)}"
-                };
+            methodDataFactory = new DataFactory(application, elem);
         }
 
         public void Stream(Type type)
         {
-            var methods = GetElementMethods(type).ToList();
+            var methods = type
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                .OrderBy(x => x.Name)
+                .ToList();
 
             if (methods.Count > 0) data.Add(new Data.MemberSeparatorWithOffset("Methods"));
 
             var currentTypeMethods = new List<string>();
 
-            foreach (MethodInfo mi in methods)
+            foreach (var methodInfo in methods)
             {
-                if (seenMethods.Contains(mi.Name)) continue;
+                if (seenMethods.Contains(methodInfo.Name)) continue;
 
-                currentTypeMethods.Add(mi.Name);
-                AddMethodToData(mi);
+                currentTypeMethods.Add(methodInfo.Name);
+
+                var methodData = GetMethodData(methodInfo);
+
+                if (methodData != null)
+                    data.Add(methodData);
             }
 
             seenMethods.AddRange(currentTypeMethods);
         }
 
-        private MethodInfo[] GetElementMethods(Type type)
-        {
-            return type
-                .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
-                .Where(IsValidMethod)
-                .OrderBy(x => x.Name)
-                .ToArray();
-        }
-
-        private bool IsValidMethod(MethodInfo methodInfo)
-        {
-            var name = $"{methodInfo.DeclaringType?.FullName}.{methodInfo.Name}";
-
-            if (methodInfo.IsSpecialName || forbiddenMethods.Contains(name))
-                return false;
-            
-            return specialMethods.Contains(name) || (!methodInfo.GetParameters().Any() && methodInfo.ReturnType != typeof(void));
-        }
-
-        private void AddMethodToData(MethodInfo methodInfo)
+        private Data.Data GetMethodData(MethodInfo methodInfo)
         {
             try
             {
-                data.Add(CreateFrom(methodInfo));
+                return methodDataFactory.Create(methodInfo);
             }
             catch (TargetException ex)
             {
-                data.Add(new Data.Exception(methodInfo.Name, ex));
+                return new Data.Exception(methodInfo.Name, ex);
             }
             catch (TargetInvocationException ex)
             {
-                data.Add(new Data.Exception(methodInfo.Name, ex));
+                return new Data.Exception(methodInfo.Name, ex);
             }
             catch (TargetParameterCountException ex)
             {
-                data.Add(new Data.Exception(methodInfo.Name, ex));
+                return new Data.Exception(methodInfo.Name, ex);
             }
-        }
-
-        private Data.Data CreateFrom(MethodInfo methodInfo)
-        {
-            object returnValue;
-
-            if (methodInfo.Name == nameof(Reference.ConvertToStableRepresentation))
-                returnValue = methodInfo.Invoke(elem, new object[] { application.ActiveUIDocument.Document });
-
-            else if (methodInfo.Name == nameof(Element.GetDependentElements))
-                returnValue = methodInfo.Invoke(elem, new object[] { null });
-
-            else
-                returnValue = methodInfo.Invoke(elem, new object[0]);
-
-            return DataTypeInfoHelper.CreateFrom(application, methodInfo, returnValue, elem);
         }
     }
 }
