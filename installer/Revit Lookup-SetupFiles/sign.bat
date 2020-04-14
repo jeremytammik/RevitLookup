67395aa081319399ae44dbd84534d5c04be6e46b
Harry Mattison <harrymattison@gmail.com>
2020-04-14 02:12:05 -0400

    Solution changes for multi-release building (#58)
    * Solution changes for multi-release building
    
    * Add MSI installer, AIP file for Advanced Installer
    
    * Sign the MSI file
    
    * Update solution
    
    Use Visual Studio 2015
    Remove extraneous configurations
    
    Co-authored-by: Harry Mattison <boostyourbim@gmail.com>

---
+++ installer/Revit Lookup-SetupFiles/sign.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Windows Kits\8.0\bin\x64\signtool.exe" sign /f "C:\Users\harry_000\Documents\Boost Your BIM\BoostYourBIM.pfx" /t "http://timestamp.comodoca.com/authenticode" /p %CodeSignPassword% /v *.msi
+pause
