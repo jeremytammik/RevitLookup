c6b36233264fc693ffc553458bd2df6daf590d36
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-10-20 20:33:02 +0200

    incremented release number

--- CS/AssemblyInfo.cs
+++ CS/AssemblyInfo.cs
@@ -150,7 +150,7 @@ using System.Reflection;
 // 2020-02-11 2020.0.0.4 incremented copyright year
 // 2020-04-12 2021.0.0.0 flat migration to Revit 2021
 // 2020-04-14 2021.0.0.1 integrated pull request #58 by @harrymattison with solution changes for multi-release building
-// 2020-10-20 2021.0.0.2 integrated pull request #63 by @swfaust to update command registration and remove obsolete test framework command
+// 2020-10-20 2021.0.0.5 integrated pull request #63 by @swfaust to update command registration and remove obsolete test framework command
 //
-[assembly: AssemblyVersion( "2021.0.0.2" )]
-[assembly: AssemblyFileVersion( "2021.0.0.2" )]
+[assembly: AssemblyVersion( "2021.0.0.5" )]
+[assembly: AssemblyFileVersion( "2021.0.0.5" )]
