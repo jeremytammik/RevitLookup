3af441bac44d5785cbbd665820a37668350c3637
michaelcoffey <michael.coffey@ideateinc.com>
2020-12-03 17:40:45 -0500

    #65 - Adds handlers for GetTemplateParameterIds and GetNonControlledTemplateParameterIds

--- CS/Snoop/CollectorExts/DataFactory.cs
+++ CS/Snoop/CollectorExts/DataFactory.cs
@@ -47,6 +47,12 @@ namespace RevitLookup.Snoop.CollectorExts
             if (declaringType == typeof(View) && methodInfo.Name == nameof(View.GetFilterVisibility))
                 return new ViewFiltersVisibilitySettings(methodInfo.Name, (View) elem);
 
+            if (declaringType == typeof(View) && methodInfo.Name == nameof(View.GetNonControlledTemplateParameterIds))
+                return new ViewGetNonControlledTemplateParameterIds(methodInfo.Name, (View)elem);
+
+            if (declaringType == typeof(View) && methodInfo.Name == nameof(View.GetTemplateParameterIds))
+                return new ViewGetTemplateParameterIds(methodInfo.Name, (View)elem);
+
             if (declaringType == typeof (Document) && methodInfo.Name == nameof(Document.Close))
                 return null;
 
