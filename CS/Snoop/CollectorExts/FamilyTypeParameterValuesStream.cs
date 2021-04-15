6a8c2e9a731053d022dd315dee9fe8ea88a2707e
Jeremy Tammik <jeremy.tammik@autodesk.com>
2021-04-15 09:47:18 +0200

    searched unsuccessfully for suitable SpecTypeId

--- CS/Snoop/CollectorExts/FamilyTypeParameterValuesStream.cs
+++ CS/Snoop/CollectorExts/FamilyTypeParameterValuesStream.cs
@@ -5,35 +5,35 @@ using Autodesk.Revit.DB;
 
 namespace RevitLookup.Snoop.CollectorExts
 {
-    public class FamilyTypeParameterValuesStream : IElementStream
+  public class FamilyTypeParameterValuesStream : IElementStream
+  {
+    private readonly ArrayList data;
+    private readonly object elem;
+
+    public FamilyTypeParameterValuesStream( ArrayList data, object elem )
     {
-        private readonly ArrayList data;
-        private readonly object elem;
-
-        public FamilyTypeParameterValuesStream(ArrayList data, object elem)
-        {
-            this.data = data;
-            this.elem = elem;
-        }
-
-        public void Stream(Type type)
-        {
-            if (type != typeof(Parameter))
-                return;
-
-            var parameter = (Parameter)elem;
-
-            var family = (parameter.Element as FamilyInstance)?.Symbol.Family ?? (parameter.Element as FamilySymbol)?.Family;
-
-            if (parameter.Definition.ParameterType != ParameterType.FamilyType || family == null)
-                return;
-
-            var familyTypeParameterValues = family
-                .GetFamilyTypeParameterValues(parameter.Id)
-                .Select(family.Document.GetElement)
-                .ToList();
-
-            data.Add(new Data.Enumerable($"{nameof(Family)}.{nameof(Family.GetFamilyTypeParameterValues)}()", familyTypeParameterValues));
-        }
+      this.data = data;
+      this.elem = elem;
     }
+
+    public void Stream( Type type )
+    {
+      if( type != typeof( Parameter ) )
+        return;
+
+      var parameter = (Parameter) elem;
+
+      var family = (parameter.Element as FamilyInstance)?.Symbol.Family ?? (parameter.Element as FamilySymbol)?.Family;
+
+      if (parameter.Definition.ParameterType != ParameterType.FamilyType || family == null) // Revit 2021
+          return;
+
+      var familyTypeParameterValues = family
+          .GetFamilyTypeParameterValues( parameter.Id )
+          .Select( family.Document.GetElement )
+          .ToList();
+
+      data.Add( new Data.Enumerable( $"{nameof( Family )}.{nameof( Family.GetFamilyTypeParameterValues )}()", familyTypeParameterValues ) );
+    }
+  }
 }
