4e073600ca373dce61a18646bd0337771e589129
mphelt <25581948+mphelt@users.noreply.github.com>
2021-09-20 16:17:39 +0200

    Update CollectorExtElement.cs

--- CS/Snoop/CollectorExts/CollectorExtElement.cs
+++ CS/Snoop/CollectorExts/CollectorExtElement.cs
@@ -81,7 +81,8 @@ namespace RevitLookup.Snoop.CollectorExts
                     new ElementMethodsStream(m_app, data, elem),
                     new SpatialElementStream(data, elem),
                     new FamilyTypeParameterValuesStream(data, elem),
-                    new ExtensibleStorageEntityContentStream(m_app.ActiveUIDocument.Document, data, elem)
+                    new ExtensibleStorageEntityContentStream(m_app.ActiveUIDocument.Document, data, elem),
+                    new PartUtilsStream(data, elem),
                 };
 
             foreach (var type in thisElementTypes)
