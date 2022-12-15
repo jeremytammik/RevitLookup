45143c177039cc62c93f5c1f2ed4b178eeebe2a0
Nice3point <nice3point@gmail.com>
2022-12-15 15:12:44 +0300

    Optimize group converter

--- RevitLookup/Views/Converters/GridViewSourceConverter.cs
+++ RevitLookup/Views/Converters/GridViewSourceConverter.cs
@@ -29,13 +29,14 @@ public sealed class GridViewSourceConverter : MarkupExtension, IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
+        var objects = (IReadOnlyList<ISnoopableObject>) value!;
+        if (objects.Count == 0) return value;
+
         var viewSource = new CollectionViewSource
         {
-            Source = (IReadOnlyList<ISnoopableObject>) value!
+            Source = objects
         };
-
         viewSource.GroupDescriptions.Add(new PropertyGroupDescription("Descriptor.Type"));
-
         return viewSource.View;
     }
 
