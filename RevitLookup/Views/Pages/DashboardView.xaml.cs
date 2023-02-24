aead04ec0dfcdfbf70dd05cbaef81090a0dafacf
Nice3point <nice3point@gmail.com>
2023-02-24 18:57:16 +0300

    Add units page

--- RevitLookup/Views/Pages/DashboardView.xaml.cs
+++ RevitLookup/Views/Pages/DashboardView.xaml.cs
@@ -18,19 +18,80 @@
 // Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
 // (Rights in Technical Data and Computer Software), as applicable.
 
+using System.Windows;
+using Autodesk.Revit.DB;
+using RevitLookup.UI.Contracts;
+using RevitLookup.UI.Controls;
 using RevitLookup.UI.Controls.Navigation;
 using RevitLookup.ViewModels.Pages;
+using RevitLookup.Views.Dialogs;
 
 namespace RevitLookup.Views.Pages;
 
 public sealed partial class DashboardView : INavigableView<DashboardViewModel>
 {
-    public DashboardView(DashboardViewModel viewModel)
+    private readonly IDialogControl _dialogControl;
+
+    public DashboardView(DashboardViewModel viewModel, IDialogService dialogService)
     {
         ViewModel = viewModel;
         InitializeComponent();
         DataContext = this;
+        Loaded += OnLoaded;
+        Unloaded += OnUnloaded;
+        _dialogControl = dialogService.GetDialogControl();
     }
 
     public DashboardViewModel ViewModel { get; }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        _dialogControl.ButtonRightClick += DialogControlOnButtonRightClick;
+    }
+
+    private void OnUnloaded(object sender, RoutedEventArgs e)
+    {
+        _dialogControl.ButtonRightClick -= DialogControlOnButtonRightClick;
+    }
+    
+    private void DialogControlOnButtonRightClick(object sender, RoutedEventArgs e)
+    {
+        _dialogControl.Hide();
+    }
+
+    private void OnClickParametersDialog(object sender, RoutedEventArgs e)
+    {
+        OpenUnitDialog(typeof(BuiltInParameter));
+    }
+
+    private void OnClickCategoriesDialog(object sender, RoutedEventArgs e)
+    {
+        OpenUnitDialog(typeof(BuiltInCategory));
+    }
+    
+    private void OnClickForgeSchemaDialog(object sender, RoutedEventArgs e)
+    {
+        OpenUnitDialog(typeof(ForgeTypeId));
+    }
+
+    private void OpenUnitDialog(Type unitType)
+    {
+        if (unitType == typeof(BuiltInParameter))
+        {
+            _dialogControl.Title = "BuiltIn Parameters";
+        }
+        else if(unitType == typeof(BuiltInCategory))
+        {
+            _dialogControl.Title = "BuiltIn Categories";
+        }
+        else if(unitType == typeof(ForgeTypeId))
+        {
+            _dialogControl.Title = "Forge Schema";
+        }
+        
+        _dialogControl.DialogWidth = 800;
+        _dialogControl.DialogHeight = 600;
+        _dialogControl.Content = new UnitsDialog(unitType);
+        _dialogControl.Show();
+    }
 }
