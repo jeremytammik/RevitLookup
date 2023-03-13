c74550e5b3a6dee0447c800c5795289cc7c17f12
Nice3point <nice3point@gmail.com>
2023-03-13 17:03:36 +0300

    Add host resolver

--- RevitLookup/Core/ComponentModel/Descriptors/HostObjectDescriptor.cs
+++ RevitLookup/Core/ComponentModel/Descriptors/HostObjectDescriptor.cs
@@ -18,19 +18,29 @@
 // Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
 // (Rights in Technical Data and Computer Software), as applicable.
 
+using System.Reflection;
 using Autodesk.Revit.DB;
 using RevitLookup.Core.Contracts;
 using RevitLookup.Core.Objects;
 
 namespace RevitLookup.Core.ComponentModel.Descriptors;
 
-public sealed class HostObjectDescriptor : ElementDescriptor, IDescriptorExtension
+public sealed class HostObjectDescriptor : ElementDescriptor, IDescriptorExtension, IDescriptorResolver
 {
     private readonly HostObject _hostObject;
 
     public HostObjectDescriptor(HostObject hostObject) : base(hostObject)
     {
         _hostObject = hostObject;
+    }
+
+    public new ResolveSet Resolve(string target, ParameterInfo[] parameters)
+    {
+        return target switch
+        {
+            nameof(HostObject.FindInserts) => ResolveSet.Append(_hostObject.FindInserts(true, true, true, true)),
+            _ => null
+        };
     }
 
     public new void RegisterExtensions(IExtensionManager manager)
