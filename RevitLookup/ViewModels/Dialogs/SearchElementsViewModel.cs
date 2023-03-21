8979839e76ed0617b2185216fda8fd45a2347359
Nice3point <nice3point@gmail.com>
2023-03-21 17:31:57 +0300

    Update search

--- RevitLookup/ViewModels/Dialogs/SearchElementsViewModel.cs
+++ RevitLookup/ViewModels/Dialogs/SearchElementsViewModel.cs
@@ -41,7 +41,7 @@ public sealed partial class SearchElementsViewModel : ObservableObject
             {
                 var delimiter = delimiters[i];
                 var split = row.Split(new[] {delimiter}, StringSplitOptions.RemoveEmptyEntries);
-                if (split.Length > 1 || i == delimiters.Length - 1)
+                if (split.Length > 1 || i == delimiters.Length - 1 || split.Length==1 && split[0] != row)
                 {
                     items.AddRange(split);
                     break;
