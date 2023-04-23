e70d4a615b0c3120b74bf67931b7bb8b0952d140
Nice3point <nice3point@gmail.com>
2023-04-23 15:39:52 +0300

    Release 2024.0.3

--- Build/Build.Properties.cs
+++ Build/Build.Properties.cs
@@ -13,9 +13,9 @@ partial class Build
 
     readonly Dictionary<string, string> VersionMap = new()
     {
-        {"Release R21", "2021.2.2"},
-        {"Release R22", "2022.2.2"},
-        {"Release R23", "2023.2.2"},
-        {"Release R24", "2024.0.2"}
+        {"Release R21", "2021.2.3"},
+        {"Release R22", "2022.2.3"},
+        {"Release R23", "2023.2.3"},
+        {"Release R24", "2024.0.3"}
     };
 }
