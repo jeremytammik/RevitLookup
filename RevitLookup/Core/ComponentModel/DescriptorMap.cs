e02b4441371d432a59274ae47148b07ee7347d20
SergeyNefyodov <nefyodov.sergei@gmail.com>
2023-11-28 12:38:00 +0400

    Parameter bindings: parameter name support at left side added

--- RevitLookup/Core/ComponentModel/DescriptorMap.cs
+++ RevitLookup/Core/ComponentModel/DescriptorMap.cs
@@ -79,6 +79,7 @@ public static class DescriptorMap
             DefinitionGroup value when type is null || type == typeof(DefinitionGroup) => new DefinitionGroupDescriptor(value),
             FamilyManager value when type is null || type == typeof(FamilyManager) => new FamilyManagerDescriptor(value),
             MEPSection value when type is null || type == typeof(MEPSection) => new MepSectionDescriptor(value),
+            KeyValuePair<Definition, object> value when type is null || type == typeof(KeyValuePair<Definition, object>) => new DefinitionBindingDescriptor(value),
             APIObject when type is null || type == typeof(APIObject) => new ApiObjectDescriptor(),
 
             //IDisposables
