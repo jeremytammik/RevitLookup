85158104f02562ab35a96ca6f26abc7b49ec7ee5
Nice3point <nice3point@gmail.com>
2024-02-10 14:22:40 +0300

    Add hosting configuration

---
+++ source/RevitLookup/Config/LoggerConfigurator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace RevitLookup.Config;
+
+public static class LoggerConfigurator
+{
+    public static void AddLoggerConfiguration(this ILoggingBuilder logging)
+    {
+        logging.AddSimpleConsole(options =>
+        {
+            options.SingleLine = true;
+            options.IncludeScopes = true;
+            options.ColorBehavior = LoggerColorBehavior.Enabled;
+        });
+    }
+}
