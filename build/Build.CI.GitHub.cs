85e8d708b4ac7f452aecea06efcd3d765a52213f
Nice3point <nice3point@gmail.com>
2024-05-01 01:44:35 +0300

    Fix changelog builder

--- build/Build.CI.GitHub.cs
+++ build/Build.CI.GitHub.cs
@@ -122,6 +122,8 @@ sealed partial class Build
 
     static void TrimEmptyLines(StringBuilder builder)
     {
+        if (builder.Length == 0) return;
+
         while (builder[^1] == '\r' || builder[^1] == '\n')
         {
             builder.Remove(builder.Length - 1, 1);
