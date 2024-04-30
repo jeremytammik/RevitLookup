9cf458b646987ef6e23511da286a60662b817d31
Nice3point <nice3point@gmail.com>
2024-04-30 12:39:56 +0300

    Add revit journal serilog sink

--- source/RevitLookup/Config/LoggerConfigurator.cs
+++ source/RevitLookup/Config/LoggerConfigurator.cs
@@ -8,27 +8,28 @@ namespace RevitLookup.Config;
 public static class LoggerConfigurator
 {
     private const string LogTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {SourceContext}: {Message:lj}{NewLine}{Exception}";
-
+    
     public static void AddSerilogConfiguration(this ILoggingBuilder builder)
     {
         var logger = CreateDefaultLogger();
         builder.AddSerilog(logger);
-
+        
         AppDomain.CurrentDomain.UnhandledException += OnOnUnhandledException;
     }
-
+    
     private static Logger CreateDefaultLogger()
     {
         return new LoggerConfiguration()
             .WriteTo.Console(LogEventLevel.Information, LogTemplate)
             .WriteTo.Debug(LogEventLevel.Debug, LogTemplate)
+            .WriteTo.RevitJournal(Context.UiApplication, restrictedToMinimumLevel: LogEventLevel.Information, outputTemplate: LogTemplate)
             .MinimumLevel.Debug()
             .CreateLogger();
     }
-
+    
     private static void OnOnUnhandledException(object sender, UnhandledExceptionEventArgs args)
     {
-        var exception = (Exception)args.ExceptionObject;
+        var exception = (Exception) args.ExceptionObject;
         var logger = Host.GetService<ILogger<AppDomain>>();
         logger.LogCritical(exception, "Domain unhandled exception");
     }
