df8b220970bb42a9e775dd516b8dbf598eafd202
Nice3point <nice3point@gmail.com>
2024-05-11 00:24:38 +0300

    Removed invalid using

--- source/RevitLookup/Core/ComponentModel/Descriptors/AssetPropertiesDescriptor.cs
+++ source/RevitLookup/Core/ComponentModel/Descriptors/AssetPropertiesDescriptor.cs
@@ -19,7 +19,6 @@
 // (Rights in Technical Data and Computer Software), as applicable.
 
 using System.Reflection;
-using System.Runtime.InteropServices.JavaScript;
 using Autodesk.Revit.DB.Visual;
 using RevitLookup.Core.Contracts;
 using RevitLookup.Core.Objects;
