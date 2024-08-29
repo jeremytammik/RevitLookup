993e1c64728733cdfd544393e60e1d5bd54a4d18
Nice3point <nice3point@gmail.com>
2024-08-30 01:48:24 +0300

    Add dependency updater

---
+++ source/DependenciesReport/DependenciesTools.cs
@@ -0,0 +1,81 @@
+﻿namespace DependenciesReport;
+
+public static class DependenciesTools
+{
+    public static Dictionary<string, Dictionary<string, string>> CreateDependenciesMap(string[] directories)
+    {
+        var dependenciesMap = new Dictionary<string, Dictionary<string, string>>();
+        foreach (var directory in directories)
+        {
+            var assemblies = Directory.GetFiles(directory, "*.dll");
+
+            foreach (var assembly in assemblies)
+            {
+                var assemblyName = Path.GetFileName(assembly);
+                var assemblyVersion = AssemblyUtils.GetAssemblyVersion(assembly);
+
+                if (!dependenciesMap.TryGetValue(assemblyName, out var value))
+                {
+                    value = new Dictionary<string, string>();
+                    dependenciesMap[assemblyName] = value;
+                }
+
+                if (assemblyVersion != null)
+                {
+                    value[Path.GetFileName(directory)] = assemblyVersion;
+                }
+            }
+        }
+
+        return dependenciesMap;
+    }
+
+    public static void UpgradeDependencies(Dictionary<string, Dictionary<string, Dictionary<string, string>>> dependenciesMaps)
+    {
+        foreach (var (yearDirectory, dependenciesMap) in dependenciesMaps)
+        {
+            foreach (var assemblyName in dependenciesMap.Keys)
+            {
+                string? maxVersion = null;
+                string? maxVersionDirectory = null;
+
+                foreach (var directory in dependenciesMap[assemblyName].Keys)
+                {
+                    var version = dependenciesMap[assemblyName][directory];
+                    if (maxVersion == null || CompareVersions(version, maxVersion) > 0)
+                    {
+                        maxVersion = version;
+                        maxVersionDirectory = directory;
+                    }
+                }
+                
+                if (maxVersionDirectory is not null && maxVersion is not null)
+                {
+                    var maxVersionFilePath = Path.Combine(yearDirectory, maxVersionDirectory, assemblyName);
+
+                    foreach (var directory in dependenciesMap[assemblyName].Keys)
+                    {
+                        if (directory != maxVersionDirectory)
+                        {
+                            var targetFilePath = Path.Combine(yearDirectory, directory, assemblyName);
+                            var targetVersion = dependenciesMap[assemblyName][directory];
+
+                            if (CompareVersions(targetVersion, maxVersion) < 0)
+                            {
+                                File.Copy(maxVersionFilePath, targetFilePath, true);
+                                Console.WriteLine($"Assembly {targetFilePath} was upgraded from version {targetVersion} to version {maxVersion}.");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static int CompareVersions(string version1, string version2)
+    {
+        var originVersion = new Version(version1);
+        var targetVersion = new Version(version2);
+        return originVersion.CompareTo(targetVersion);
+    }
+}
--- source/DependenciesReport/Program.cs
+++ source/DependenciesReport/Program.cs
@@ -6,22 +6,50 @@ var userFolder = Environment.GetFolderPath(Environment.SpecialFolder.Application
 var addinsPath = Path.Combine(userFolder, "Autodesk", "Revit", "Addins");
 var yearsDirectories = Directory.GetDirectories(addinsPath);
 
-var reportName = $"Dependencies report - {DateTime.Now.ToString("yyyy-MM-dd", DateTimeFormatInfo.InvariantInfo)}.txt";
+var reportName = $"DependenciesReport-{DateTime.Now.ToString("yyyy-MM-dd", DateTimeFormatInfo.InvariantInfo)}.txt";
 var reportPath = Path.Combine(Path.GetTempPath(), reportName);
 var summaryWriter = new SummaryWriter(reportPath);
 
+var dependenciesMaps = new Dictionary<string, Dictionary<string, Dictionary<string, string>>>();
 foreach (var yearDirectory in yearsDirectories)
 {
     summaryWriter.Write($"Revit version: {Path.GetFileName(yearDirectory)}");
     summaryWriter.WriteLine();
-    
+
     var directories = Directory.GetDirectories(yearDirectory);
-    var dependenciesMap = DependenciesParser.CreateDependenciesMap(directories);
+    var dependenciesMap = DependenciesTools.CreateDependenciesMap(directories);
     var dependenciesTable = TableFormater.CreateReportTable(directories, dependenciesMap);
-    
+    dependenciesMaps.Add(yearDirectory, dependenciesMap);
+
     summaryWriter.Write(dependenciesTable.ToMinimalString());
     summaryWriter.WriteLine();
 }
 
 summaryWriter.Save();
-Process.Start("explorer.exe", reportPath);
+
+Console.WriteLine($"Dependencies report saved: {reportPath}");
+Console.WriteLine(@"Open the report? Y\N");
+var response = Console.ReadLine();
+
+if (string.Compare(response, "Y", StringComparison.OrdinalIgnoreCase) == 0)
+{
+    Process.Start(new ProcessStartInfo
+    {
+        FileName = "explorer.exe",
+        Arguments = reportPath,
+        CreateNoWindow = true,
+        UseShellExecute = true
+    });
+}
+
+Console.WriteLine(@"Try to fix dependencies and upgrade to the latest version? Y\N");
+response = Console.ReadLine();
+
+if (string.Compare(response, "Y", StringComparison.OrdinalIgnoreCase) == 0)
+{
+    DependenciesTools.UpgradeDependencies(dependenciesMaps);
+    Console.WriteLine("Success!");
+}
+
+Console.WriteLine("You can now close this terminal with Ctrl+C");
+Console.ReadLine();
